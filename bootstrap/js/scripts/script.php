7140fe79dd81fef8f1f15e3cf7dd419ada8e7aa8
EvaneiFreitas <nwillian533@gmail.com>
2020-05-26 11:25:54 -0300

    adicionando validações de campos do formulario

---
+++ bootstrap/js/scripts/script.php
@@ -0,0 +1,76 @@
+
+<?php
+$categorias = [];
+$categorias[] = 'Infantil';
+$categorias[] = 'Adolescente';
+$categorias[] = 'Adulto';
+$categorias[] = 'Idoso';
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+//var_dump($nome);
+//var_dump($idade);
+//return 0;
+
+
+if(empty($nome)){
+ echo "Você deve digitar um Nome!!!";
+ return;
+}
+
+if(strlen($nome) <= 2 ){
+  echo "O Nome tem que ter mais de duas letras";
+  return;
+}
+
+if(strlen($nome)  > 40){
+   echo " nome muito grande!";
+   return;
+}
+
+
+
+if($idade <= 6 && $idade <= 12)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'Infantil')
+        
+          echo "O Nadador   " .$nome. "    Compete na categoria Infantil";
+        
+    }
+}
+else if($idade >= 13 && $idade <= 20)
+    {
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+            if($categorias[$i] == 'Adolescente')
+            
+              echo "O Nadador ".$nome. "Compete na categoria Adolescente ";
+            
+        }
+    }
+
+    else if($idade >= 21 && $idade <= 49)
+    {
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+            if($categorias[$i] == 'Adulto')
+            
+              echo "O Nadador ".$nome. "Compete na categoria Adulto ";
+            
+        }
+    }
+
+    else
+    {
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+            if($categorias[$i] == 'Idoso')
+            
+              echo "O Nadador ".$nome. "Compete na categoria Idoso";
+            
+        }
+    }
+?>
